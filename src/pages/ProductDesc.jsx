89696fd1c99b68077fb88987800957e621a6ff97
sixdgt <c4crypt@gmail.com>
2023-03-02 08:58:07 +0545

    data fetching with api

--- src/pages/ProductDesc.jsx
+++ src/pages/ProductDesc.jsx
@@ -1,5 +1,7 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import ProductDetail from '../components/ProductDetail'
+import Axios from 'axios'
+import {Container, Card, Row, Col} from 'react-bootstrap'
 
 export default function ProductDesc() {
     const item = {
@@ -14,9 +16,29 @@ export default function ProductDesc() {
         contact: 98765431210
     }
 
+    const [products, setProducts] = useState([]);
+
+    const fetchProducts = async() => {
+        const { data } = await Axios.get('https://dummyjson.com/products');
+        const products = data;
+        setProducts(products);
+        console.log(products);
+    }
+
+    useEffect(() => {
+        fetchProducts();
+    }, []);
+
     return (
         <div>
             <ProductDetail product={item} customer={customerDetail} />
+            <Container>
+                <Row>
+                {/* {products.map((product) => {
+                    <h1>{product.title}</h1>
+                })} */}
+                </Row>
+            </Container>
         </div>
     )
 }
